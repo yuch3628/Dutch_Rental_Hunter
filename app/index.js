4f16bb498530b68972fc5d6b99f485205eec370d
yuch3628 <sf10126@yahoo.com.tw>
2024-10-21 15:29:47 +0200

    Enhance db logic

--- app/index.js
+++ app/index.js
@@ -1,14 +1,20 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import scheduleHouseScraping from "./services/housescraping.js"
+dotenv.config();
 
 const app = express();
 app.use(cors());
 
+await scheduleHouseScraping();
+
+
 app.get("/", (req,res) => { 
     res.send("successfully to connect!");
-}
+});
 
-);
-app.listen(8800, ()=>{
-    console.log("Server running on port 8800")
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server running on port ${process.env.PORT}`)
 })

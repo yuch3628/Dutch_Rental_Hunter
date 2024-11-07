26a77a47b6c8cb20d483e64cf5bcbd67e905b565
yuch3628 <sf10126@yahoo.com.tw>
2024-11-07 14:24:36 +0100

    Refactor

---
+++ app/services/dbHandler.js
@@ -0,0 +1,38 @@
+import dotenv from "dotenv";
+dotenv.config();
+import pg from "pg";
+
+export const db = new pg.Pool({
+    user: process.env.POSTGRES_USERNAME,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+});
+
+export async function checkHouseExist(house) {
+    let isExisted = await db.query("SELECT EXISTS(SELECT 1 FROM house WHERE address = $1);", [house]);
+    return isExisted.rows[0].exists;
+}
+
+export async function createHouseInfo(houseData) {
+    return await db.query("INSERT INTO house(address, city, region, postcode, price, url) VALUES ($1, $2, $3, $4, $5, $6);", [houseData.name, houseData.city, houseData.region, houseData.postcode, houseData.price, houseData.url]);
+}
+
+export async function createHouseImage(imgName, imgUrl) {
+    return await db.query("INSERT INTO houseimage(address, imgurl) VALUES ($1, $2);", [imgName, imgUrl]);
+}
+
+export async function getExistHouse(house){
+    var existedData = await db.query('SELECT * FROM house WHERE address = ($1);',[house]);
+
+    return existedData.rows;
+}
+
+export async function getHouseInfoByDate(date) {
+    let house = await db.query("SELECT * FROM house WHERE posting_date = ($1);",[date]);
+    return house.rows;
+}
--- app/services/housescraping.js
+++ app/services/housescraping.js
@@ -1,51 +1,44 @@
 import {PythonShell} from "python-shell";
-import pg from "pg";
-import dotenv from "dotenv";
-dotenv.config();
-
-const db = new pg.Client({
-    user: process.env.POSTGRES_USERNAME,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATABASE,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT
-  });
+import {db, createHouseInfo, createHouseImage, checkHouseExist, getExistHouse} from "./dbHandler.js"
 
 
 async function scheduleHouseScraping() {
     let data;
 
     await PythonShell.run('./services/rentalscraper.py', null).then(messages => {
-    data = JSON.parse(messages[0])}); 
 
+    data = JSON.parse(messages[0])}); 
+    
+    if (data.length == null) {
+        return {};
+    }
     var existedRent = [];
     var dbError = [];
 
-    await db.connect();
+    // await db.connect();
    
     for(let i in data) {
-        let isExisted = await db.query("SELECT EXISTS(SELECT 1 FROM house WHERE address = $1);", [data[i].name]);
-        if(!isExisted.rows[0].exists) {
+        let isExisted = await checkHouseExist(data[i].name);
+
+        if(!isExisted) {
             try {
-                await db.query("INSERT INTO house(address, city, region, postcode, price) VALUES ($1, $2, $3, $4, $5);", [data[i].name, data[i].city, data[i].region, data[i].postcode, data[i].price]);
+                await createHouseInfo(data[i]);
             } catch (err){
                 dbError.push(err);
             }
             for (let j in data[i].imgUrl) {
                 try {
-                    await db.query("INSERT INTO houseimage(address, imgurl) VALUES ($1, $2);", [data[i].name, data[i].imgUrl[j]]);
+                    await createHouseImage(data[i].name, data[i].imgUrl[j]);
                 } catch (err) {
                     dbError.push(err);
                 }
-            
             }
         } else {
-            var existedData = await db.query('SELECT address, city, region, postcode, price, posting_date FROM house WHERE address = ($1);',[(data[i].name)]);
-            existedRent.push(existedData.rows[0]);
-
+            var existedData = await getExistHouse(data[i].name);
+            existedRent.push(existedData);
         }    
     }
-    await db.end();
+    // await db.end();
     
     if (existedRent.length) {
         console.log('Already have in DB:');

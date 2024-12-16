a4783f41ab1ed4b3e5a74ffa4654e7db0e561e92
yuch3628 <sf10126@yahoo.com.tw>
2024-12-16 22:40:30 +0100

    add an api

--- app/index.js
+++ app/index.js
@@ -68,6 +68,73 @@ app.get("/house/date/:id", async (req,res) => {
     let house = await getHouseInfoByDate(prevDate);
     res.send(house);
 });
+
+app.get('/dashboard', async(req,res) => {
+    const date = new Date();
+    let prevDate = new Date(date);
+    prevDate.setMonth(date.getMonth() - 1);
+    let house = await getHouseInfoByDate(prevDate);
+
+    const area = {
+        central: 0,
+        east: 0,
+        north: 0,
+        westpoort: 0,
+        west: 0,
+        newwest: 0,
+        south: 0,
+        southeast: 0,
+        others:0 
+    };
+
+    for (let info of house){
+        let code = parseInt(info.postcode.split(' ')[0]);
+        switch (true) {
+            case (code < 1019):
+                area.central +=1;
+                break;
+            case (code <= 1019):
+                area.east +=1;
+                break;
+            case (code >= 1020 && code < 1040):
+                area.north +=1;
+                break;
+            case (code >= 1040 && code < 1050):
+                area.westpoort +=1;
+                break;
+            case (code >= 1050 && code < 1060):
+                area.west +=1;
+                break;
+            case (code >= 1060 && code < 1070):
+                area.newwest +=1;
+                break;
+            case (code >= 1070 && code < 1084):
+                area.south +=1;
+                break;
+            case (code >= 1086 && code < 1100):
+                area.east +=1;
+                break;
+            case (code >= 1100 && code < 1110):
+                area.southeast +=1;
+                break;
+            default:
+                area.others +=1;
+                break;
+        }
+    }
+
+    let data = [];
+    let count = 0;
+    for (let a in area){
+        let item = {id:count, value:area[a], label:a};
+        data.push(item);
+        count++;
+    }
+
+    res.send(data);
+});  
+
+
 app.listen(process.env.PORT, ()=>{
     console.log(`Server running on port ${process.env.PORT}`)
 })

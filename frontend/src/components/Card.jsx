466381d34d49e2d2b9f0afe172d139e6450a7b2b
yuch3628 <sf10126@yahoo.com.tw>
2025-04-01 17:13:48 +0200

    Remove non used code

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
